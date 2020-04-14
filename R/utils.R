4753b8df8eabac7f6f710dfbf2612a11ab173fb9
Craig Brinkerhoff <cbrinkerhoff@umass.edu>
2020-04-14 12:54:19 -0400

    New logit model for unsupervised river types

--- R/utils.R
+++ R/utils.R
@@ -61,18 +61,17 @@ classify_func <- function(Wobs) {
 #' @param Wobs observed widths matrix
 classify_func_unsupervised <- function(Wobs) {
   width <- median((Wobs), na.rm = TRUE) #note these are not log widths!
-  width_sd <- sd((Wobs), na.rm = TRUE)
 
   #One-vs-Rest Logistic regression model
   #test set accuracy rate of 87%
-  p_noise <- 1 / (1.0 + exp(-(-3.12907956 + 7.72682055e-04 * width_sd + 1.68285442e-03 * width)))
-  p_1 <- 1 / (1.0 + exp(-(1.9719785 + -7.08487404e-04 * width_sd + -9.57359172e-04 * width)))
-  p_2 <- 1 / (1.0 + exp(-(-4.0504766  + -1.30605126e-04 * width_sd + 1.60069188e-03 * width)))
-  p_3<- 1 / (1.0 + exp(-(-4.34955267 + -6.70236303e-03 * width_sd + 7.59492911e-05 * width)))
-  p_4 <- 1 / (1.0 + exp(-(-3.9977568  + 3.24040207e-04 * width_sd + 1.59933716e-03 * width)))
-  p_5 <- 1 / (1.0 + exp(-(-4.26058253 + -2.39296957e-03 * width_sd + -1.41460664e-02 * width)))
-  p_6 <- 1 / (1.0 + exp(-(-3.86984885 + -2.63864258e-04 * width_sd + -1.68608848e-03 * width)))
-  p_7 <- 1 / (1.0 + exp(-(-1.15978639 + 5.68264551e-04 * width_sd + -1.27528508e-01 * width)))
+  p_noise <- 1 / (1.0 + exp(-(-3.11056777 + 0.00189261 * width)))
+  p_1 <- 1 / (1.0 + exp(-(1.95763357 + -0.00114445 * width)))
+  p_2 <- 1 / (1.0 + exp(-(-4.05274822 + 0.00156913 * width)))
+  p_3<- 1 / (1.0 + exp(-(-4.43158034 + -0.00116211 * width)))
+  p_4 <- 1 / (1.0 + exp(-(-3.99272449 + 0.00168791* width)))
+  p_5 <- 1 / (1.0 + exp(-(-4.28204232 + -0.01493184 * width)))
+  p_6 <- 1 / (1.0 + exp(-(-3.87408245 + -0.00175089 * width)))
+  p_7 <- 1 / (1.0 + exp(-(-1.16209678 + -0.12664823 * width)))
 
   probs <- data.frame(p_noise, p_1, p_2, p_3, p_4, p_5, p_6, p_7)
 
